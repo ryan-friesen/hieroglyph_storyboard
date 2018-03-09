38df6edbf27008d33f82b135d09afba68cad1474
Ryan Friesen <viniloruido@gmail.com>
2018-03-08 22:35:58 -0800

    Updates
    This is the next step: https://www.redips.net/my/preview/REDIPS_drag/example03/index_static.html

--- heiroglyph_app/main.js
+++ heiroglyph_app/main.js
@@ -1,7 +1,18 @@
 function dragUser(user, event) {
-    event.dataTransfer.setData('User', user.id);
+
+    event.dataTransfer.setData('User', user.id);		
+
 }
 function dropUser(target, event) {
     var user = event.dataTransfer.getData('User');
-    target.appendChild(document.getElementById(user)); 
+    target.appendChild(document.getElementById(user)); 	
+
+}
+
+function cloneUser(user, event) {
+	
+	var user = event.dataTransfer.getData('User');
+	user1 = user.cloneNode(true);
+	user.parentNode.appendChild(user1);
+	
 }
